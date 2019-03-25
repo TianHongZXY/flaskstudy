176e856d5753c18cba8a393e55f8fc07603eef76
Jason Zhu <zhuxinyu886@163.com>
2019-03-25 13:58:25 +0800

    Update app.py

--- hello/app.py
+++ hello/app.py
@@ -11,18 +11,23 @@ bootstrap = Bootstrap()
 bootstrap.init_app(app)
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
 @app.route('/hello')
 def hello():
     name = g.name
-    if name is None:
-        name = request.cookies.get('name', 'human')
-
-        response = '<h1>Hello %s!</h1>' %escape(name)
+    if name is not None:
+        # name = request.cookies.get('name', 'human')
+        # name = request.args.get('name', 'human')
+        response = '<h1>Hello %s!</h1>' % escape(name)
 
         if 'logged_in' in session:
             response += '[Authenticated]'
         else:
             response += '[Not Authenticated]'
+    else:
+        response = '<h1>Hello!</h1>'
     return response
 
 
@@ -176,4 +181,25 @@ def load_post():
 def inject_foo():
     foo = 'You are a foo.'
     # foo变量在模板中可以直接使用
+movies = [
+    {'name': 'My Neighbor Totoro', 'year': '1988'},
+    {'name': 'Three Colours trilogy', 'year': '1993'},
+    {'name': 'Forrest Gump', 'year': '1994'},
+    {'name': 'Perfect Blue', 'year': '1997'},
+    {'name': 'The Matrix', 'year': '1999'},
+    {'name': 'Memento', 'year': '2000'},
+    {'name': 'The Bucket list', 'year': '2007'},
+    {'name': 'Black Swan', 'year': '2010'},
+    {'name': 'Gone Girl', 'year': '2014'},
+    {'name': 'CoCo', 'year': '2017'},
+]
+
+@app.route('/watchlist')
+def watchlist():
+    return render_template('watchlist.html',user=None, movies=movies)
+
+# 模板上下文处理函数，它返回的值在任意一个模板中都可用
+@app.context_processor
+def inject_foo():
+    foo = 'You are a foo.'
     return {'foo':foo}
